d15ab9531fb21b7c9ee3b30903b5220d5ab2bfc1
jungwonqv <jungwonqv@localhost>
2023-10-01 16:34:31 +0900

    - Plays rock, paper, scissor

---
+++ RockPaperScissor/src/FirstGen.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+public class FirstGen {
+
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		System.out.print("Welcome, please pick between Rock, Paper and Scissor. ");
+		
+		String humanChoice = console.next();
+		String computerChoice = computerChoiceGenerator();
+		
+		System.out.println("The computer picked " + computerChoice + ".");
+		winDrawLoss(computerChoice, humanChoice);
+	}
+	
+	public static void winDrawLoss(String computerChoice, String humanChoice) {
+		if(computerChoice.equals("Rock") && humanChoice.equals("Rock")) {
+			System.out.print("It is a draw!");
+		}
+		if(computerChoice.equals("Rock") && humanChoice.equals("Paper")) {
+			System.out.print("You won!");
+		}
+		if(computerChoice.equals("Rock") && humanChoice.equals("Scissor")) {
+			System.out.print("You lost...");
+		}
+		if(computerChoice.equals("Paper") && humanChoice.equals("Rock")) {
+			System.out.print("You lost...");
+		}
+		if(computerChoice.equals("Paper") && humanChoice.equals("Paper")) {
+			System.out.print("It is a draw!");
+		}
+		if(computerChoice.equals("Paper") && humanChoice.equals("Scissor")) {
+			System.out.print("You won!");
+		}
+		if(computerChoice.equals("Scissor") && humanChoice.equals("Rock")) {
+			System.out.print("You won!");
+		}
+		if(computerChoice.equals("Scissor") && humanChoice.equals("Paper")) {
+			System.out.print("You lost...");
+		}
+		if(computerChoice.equals("Scissor") && humanChoice.equals("Scissor")) {
+			System.out.print("It is a draw!");
+		}
+	}
+	
+	public static String computerChoiceGenerator() {
+		double randomNum = Math.random();
+		String computerChoice = "";
+		if(randomNum < (double) 1/3) {
+			computerChoice = "Rock";
+		} else if(randomNum < (double) 2/3) {
+			computerChoice = "Paper";
+		} else {
+			computerChoice = "Scissor";
+		}
+		return computerChoice;
+	}
+
+}
