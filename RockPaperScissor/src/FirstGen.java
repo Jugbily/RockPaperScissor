b08bf9c0a75b3d36fb5de50cbac8d68c30550825
jungwonqv <jungwonqv@localhost>
2023-12-01 21:25:11 +0900

    The choices (rock, paper, scissors) became more fair.
    You can play again directly without restarting the program.

--- RockPaperScissor/src/FirstGen.java
+++ RockPaperScissor/src/FirstGen.java
@@ -1,33 +1,43 @@
 import java.util.Scanner;
-
+import java.util.Random;
+//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
 public class FirstGen {
-	public static final String password = "123";
+	public static final String password = "password";
 	
 	public static void main(String[] args) {
 		Scanner console = new Scanner(System.in);
-		
-		System.out.print("Please type the password to play. ");
-		String humanPassword = console.next();
-		console.nextLine();
-		if (!(humanPassword.equals(password))) {
-			System.out.print("The password in incorrect, please restart the program.");
-		} else {
-
-			intro(console);
-
-			String humanChoice = console.next();
-			String computerChoice = computerChoiceGenerator();
-
-			humanChoice = humanChoice.toLowerCase();
-			computerChoice = computerChoice.toLowerCase();
-
-			if (!(humanChoice.equals("rock") || humanChoice.equals("paper") || humanChoice.equals("scissor"))) {
-				System.out.print("Restart the program, \"" + humanChoice + "\" is not a valid charater.");
+		int again;
+		do {
+			System.out.print("Please type the password to play. ");
+			String humanPassword = console.next();
+			console.nextLine();
+			if (!(humanPassword.equals(password))) {
+				System.out.println("The password in incorrect.");
 			} else {
-				System.out.println("You picked " + humanChoice + " and the computer picked " + computerChoice + ".");
-				winDrawLoss(computerChoice, humanChoice);
+				
+				intro(console);
+					
+					String humanChoice = console.next();
+					console.nextLine();
+					String computerChoice = choiceGen();
+		
+					humanChoice = humanChoice.toLowerCase();
+					computerChoice = computerChoice.toLowerCase();
+		
+					if (humanChoice.equals("rock") || humanChoice.equals("paper") || humanChoice.equals("scissors")) {
+						System.out.println("You picked " + humanChoice + " and the computer picked " + computerChoice + ".");
+						winDrawLoss(computerChoice, humanChoice);
+					} else {
+						System.out.println("You picked " + humanChoice + " and the computer picked " + computerChoice + ".");
+						winDrawLoss(computerChoice, humanChoice);
+					}
 			}
-		}
+			System.out.print("Type 1 if you want to play again.");
+			again = console.nextInt();
+		} while (again == 1); 
+		
+		
+		
 	}
 
 	// Welcomes the player by asking the name and tells the rules of the game.
@@ -35,52 +45,52 @@ public class FirstGen {
 		System.out.print("Welcome! Please state your name in order to play. ");
 		String humanName = console.nextLine();
 		System.out.print("Hello " + humanName + ", "
-				+ "please pick between rock, paper, and scissor. (Only the first word counts) ");
+				+ "please pick between rock, paper, and scissors. (Only the first word counts) ");
 	}
 
 	// Calculate the possibility and tells you your results.
 	public static void winDrawLoss(String computerChoice, String humanChoice) {
 		if (computerChoice.equals("rock") && humanChoice.equals("rock")) {
-			System.out.print("It is a draw!");
+			System.out.println("It is a draw!");
 		}
 		if (computerChoice.equals("rock") && humanChoice.equals("paper")) {
-			System.out.print("You won!");
+			System.out.println("You won!");
 		}
-		if (computerChoice.equals("rock") && humanChoice.equals("scissor")) {
-			System.out.print("You lost...");
+		if (computerChoice.equals("rock") && humanChoice.equals("scissors")) {
+			System.out.println("You lost...");
 		}
 		if (computerChoice.equals("paper") && humanChoice.equals("rock")) {
-			System.out.print("You lost...");
+			System.out.println("You lost...");
 		}
 		if (computerChoice.equals("paper") && humanChoice.equals("paper")) {
-			System.out.print("It is a draw!");
+			System.out.println("It is a draw!");
 		}
-		if (computerChoice.equals("paper") && humanChoice.equals("scissor")) {
-			System.out.print("You won!");
+		if (computerChoice.equals("paper") && humanChoice.equals("scissors")) {
+			System.out.println("You won!");
 		}
-		if (computerChoice.equals("scissor") && humanChoice.equals("rock")) {
-			System.out.print("You won!");
+		if (computerChoice.equals("scissors") && humanChoice.equals("rock")) {
+			System.out.println("You won!");
 		}
-		if (computerChoice.equals("scissor") && humanChoice.equals("paper")) {
-			System.out.print("You lost...");
+		if (computerChoice.equals("scissors") && humanChoice.equals("paper")) {
+			System.out.println("You lost...");
 		}
-		if (computerChoice.equals("scissor") && humanChoice.equals("scissor")) {
-			System.out.print("It is a draw!");
+		if (computerChoice.equals("scissors") && humanChoice.equals("scissors")) {
+			System.out.println("It is a draw!");
 		}
 	}
 
 	// Selects a random number and randomly picks a choice.
-	public static String computerChoiceGenerator() {
-		double randomNum = Math.random();
-		String computerChoice = "";
-		if (randomNum < (double) 1 / 3) {
-			computerChoice = "Rock";
-		} else if (randomNum < (double) 2 / 3) {
-			computerChoice = "Paper";
-		} else {
-			computerChoice = "Scissor";
+	public static String choiceGen() {
+		Random rand = new Random();
+		int randnum = rand.nextInt(3);
+		if (randnum == 0) {
+			return "scissors";
 		}
-		return computerChoice;
+		if (randnum == 1) {
+			return "rock";
+		} else {
+			return "paper";
+		}
 	}
 
 }
